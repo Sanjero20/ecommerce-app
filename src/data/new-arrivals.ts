c22acf4a78960a52b8131926ea21e6335f853b8c
Sanjero20 <gelosantos72@gmail.com>
2024-08-12 12:29:53 +0800

    refactor: add id on product type

--- src/data/new-arrivals.ts
+++ src/data/new-arrivals.ts
@@ -1,25 +1,20 @@
-import { StaticImageData } from "next/image";
 import product1 from "../../public/images/new-arrivals/1.png";
 import product2 from "../../public/images/new-arrivals/2.png";
 import product3 from "../../public/images/new-arrivals/3.png";
 import product4 from "../../public/images/new-arrivals/4.png";
 
-type Product = {
-  name: string;
-  src: StaticImageData;
-  rating: number;
-  price: number;
-  discount?: number;
-};
+import { Product } from "@/types/index.types";
 
 export const NEW_ARRIVALS: Product[] = [
   {
+    id: 0,
     name: "T-Shirt with Tape Details",
     src: product1,
     rating: 4.5,
     price: 120,
   },
   {
+    id: 1,
     name: "Skinny Fit Jeans",
     src: product2,
     rating: 3.5,
@@ -27,12 +22,14 @@ export const NEW_ARRIVALS: Product[] = [
     discount: 20,
   },
   {
+    id: 2,
     name: "Checkered Shirt",
     src: product3,
     rating: 4.5,
     price: 180,
   },
   {
+    id: 3,
     name: "Sleeve Striped T-shirt",
     src: product4,
     rating: 4.5,
