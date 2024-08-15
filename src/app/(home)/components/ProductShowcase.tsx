ca69cf6f0b206dfba8abd3d7b69dd6796cd29ef1
Sanjero20 <gelosantos72@gmail.com>
2024-08-15 08:18:37 +0800

    refactor: reuse new arrivals component layout

--- src/app/(home)/components/ProductShowcase.tsx
+++ src/app/(home)/components/ProductShowcase.tsx
@@ -1,18 +1,20 @@
 import ProductCard from "@/components/ProductCard";
-import Card from "@/components/ui/Card";
-import { NEW_ARRIVALS } from "@/data/new-arrivals";
-import Image from "next/image";
+import { Product } from "@/types/index.types";
 
-const NewArrivals = () => {
+interface Props {
+  title: string;
+  products: Product[];
+}
+
+const ProductShowcase = ({ title, products }: Props) => {
   return (
     <div className="container flex flex-col gap-4 px-4 py-8">
       <h2 className="mb-4 text-center font-integral text-3xl uppercase lg:text-5xl">
-        new arrivals
+        {title}
       </h2>
 
-      {/*  */}
       <div className="flex snap-x gap-8 overflow-x-scroll md:overflow-x-auto">
-        {NEW_ARRIVALS.map((product) => (
+        {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
@@ -20,10 +22,8 @@ const NewArrivals = () => {
       <button className="mx-auto w-full rounded-full border border-gray-300 bg-transparent p-4 sm:max-w-48">
         View All
       </button>
-
-      <hr />
     </div>
   );
 };
 
-export default NewArrivals;
+export default ProductShowcase;
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,13 +1,18 @@
 import HeroSection from "./components/Hero";
 import Brands from "./components/Brands";
-import NewArrivals from "./components/NewArrivals";
+import ProductShowcase from "./components/ProductShowcase";
+
+import { NEW_ARRIVALS } from "@/data/new-arrivals";
 
 const HomePage = () => {
   return (
     <div>
       <HeroSection />
       <Brands />
-      <NewArrivals />
+
+      <ProductShowcase title="new arrivals" products={NEW_ARRIVALS} />
+      <hr className="container" />
+      <ProductShowcase title="top selling" products={NEW_ARRIVALS} />
     </div>
   );
 };
