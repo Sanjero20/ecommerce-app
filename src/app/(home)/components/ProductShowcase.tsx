b7b18214839b38c12ec0674143fd59f153c3160d
Sanjero20 <gelosantos72@gmail.com>
2024-08-23 12:28:29 +0800

    feat: use swiper js for product showcase

--- src/app/(home)/components/ProductShowcase.tsx
+++ src/app/(home)/components/ProductShowcase.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import ProductCard from "@/components/ProductCard";
 import Button from "@/components/ui/Button";
 import { Product } from "@/types/index.types";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Scrollbar, A11y } from "swiper/modules";
+import "swiper/css";
 
 interface Props {
   title: string;
@@ -12,11 +18,30 @@ const ProductShowcase = ({ title, products }: Props) => {
     <div className="flex flex-col gap-8 px-4 py-12">
       <h2 className="mb-4 text-center">{title}</h2>
 
-      <div className="flex snap-x gap-8 overflow-x-scroll md:overflow-x-auto">
+      <Swiper
+        className="w-full"
+        modules={[Navigation, Scrollbar, A11y]}
+        spaceBetween={25}
+        breakpoints={{
+          320: {
+            slidesPerView: 1, // Mobile
+          },
+          640: {
+            slidesPerView: 2, // Tablet
+          },
+          768: {
+            slidesPerView: 4, // Desktop
+          },
+        }}
+        navigation
+        scrollbar={{ draggable: true }}
+      >
         {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
+          <SwiperSlide key={product.id}>
+            <ProductCard key={product.id} product={product} />
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
 
       <Button className="mx-auto w-full sm:max-w-48" variant="outline">
         View All
