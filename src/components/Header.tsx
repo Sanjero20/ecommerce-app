751c5a664e13cb3c1b4ae1e028525c10e171ff13
Sanjero20 <gelosantos72@gmail.com>
2024-08-26 14:17:51 +0800

    feat: add page navigation via header links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import Input from "./ui/Input";
 import { Icon } from "./ui/Icon";
 
@@ -13,17 +14,21 @@ const Header = () => {
           </div>
 
           {/* Mobile Logo */}
-          <div className="font-integral text-2xl">SHOP.CO</div>
+          <Link href="/">
+            <div className="font-integral text-2xl">SHOP.CO</div>
+          </Link>
         </div>
 
         {/* Desktop Logo */}
-        <Image
-          src={"/images/logo.svg"}
-          className="hidden lg:block"
-          width={135}
-          height={40}
-          alt="logo"
-        />
+        <Link href="/">
+          <Image
+            src={"/images/logo.svg"}
+            className="hidden lg:block"
+            width={135}
+            height={40}
+            alt="logo"
+          />
+        </Link>
 
         {/* Navbar */}
         <nav className="hidden lg:block">
@@ -32,9 +37,15 @@ const Header = () => {
               Shop
               <Icon src={"/icons/caret-down.svg"} size={16} />
             </li>
-            <li>On Sale</li>
-            <li>New Arrivals</li>
-            <li>Brands</li>
+            <li>
+              <Link href="/on-sale">On Sale</Link>
+            </li>
+            <li>
+              <Link href="/new-arrivals">New Arrivals</Link>
+            </li>
+            <li>
+              <Link href="/brands">Brands</Link>
+            </li>
           </ul>
         </nav>
       </div>
